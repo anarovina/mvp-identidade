892e1e86e9dcce65953e9978bfe54eac3c188315
Ana Rovina <analucia.rovina@gmail.com>
2026-01-21 17:38:08 -0300

    Add base path for GitHub Pages repository

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mvp-identidade/',
   plugins: [react()],
   server: {
     allowedHosts: ['marca.arovinastudio.com.br', 'wontedly-dismissible-soo.ngrok-free.dev', '*.ngrok-free.dev'],
